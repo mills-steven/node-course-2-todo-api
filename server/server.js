b8f05a939c4972f270c4ec5522f3150890f35e01
Steve Mills <stevepatrickmills@gmail.com>
2017-04-02 14:50:38 -0500

    Add get todos route

--- server/server.js
+++ server/server.js
@@ -34,7 +34,13 @@ app.post('/todos', (req, res) =>{
 // });
 
 //GET
-
+app.get('/todos', (req, res) => {
+  Todo.find().then((todos) => {
+    res.send({todos});
+  }, (e) => {
+    res.status(400).send(e);
+  });
+});
 
 
 //DELETE
