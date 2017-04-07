cc5f40c71deab6d9c28d88cd527ac008b1dc4379
Steve Mills <stevepatrickmills@gmail.com>
2017-04-06 21:48:11 -0500

    Add DELETE to todo app

--- server/server.js
+++ server/server.js
@@ -54,7 +54,7 @@ app.get('/todos/:id', (req, res) => {
   Todo.findById(id).then((todo) => {
     if (!todo) {
       return res.status(404).send();
-    }
+    };
       res.status(200).send({todo});
     }).catch((e) =>{
       res.status(400).send();
@@ -62,9 +62,22 @@ app.get('/todos/:id', (req, res) => {
 });
 
 
-
 //DELETE
+app.delete('/todos/:id', (req, res) => {
+  var id = req.params.id;
 
+  if(!ObjectID.isValid(id)) {
+    return res.status(404).send();
+  };
+  Todo.findByIdAndRemove(id).then((todo) =>{
+    if(!todo){
+      return res.status(404).send();
+    };
+    res.status(200).send({todo});
+  }).catch((e) => {
+    res.status(400).send();
+  });
+});
 
 
 app.listen(port, () => {
